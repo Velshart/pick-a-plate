33149bf656057fc1dd3fb9eafc907e4241ef8c0d
michal <pkot2003@gmail.com>
2023-09-08 22:37:23 +0200

    -add word files to the project -TextFileService now reads both files' content

--- src/main/java/me/velshh/pickaplate/service/TextFileService.java
+++ src/main/java/me/velshh/pickaplate/service/TextFileService.java
@@ -1,4 +1,38 @@
 package me.velshh.pickaplate.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
 public class TextFileService {
+    private List<String> englishWords;
+    private List<String> polishWords;
+
+    public TextFileService() {
+        loadFiles();
+    }
+
+    private void loadFiles() {
+        Path englishWordsFilePath = Path.of("englishwords.txt");
+        Path polishWordsFilePath = Path.of("polishwords.txt");
+        try {
+            this.englishWords = Files.readAllLines(englishWordsFilePath);
+            this.polishWords = Files.readAllLines(polishWordsFilePath);
+
+        }catch (IOException ex) {
+            this.englishWords = Collections.emptyList();
+            this.polishWords = Collections.emptyList();
+
+        }
+    }
+
+    public List<String> getEnglishWords() {
+        return englishWords;
+    }
+
+    public List<String> getPolishWords() {
+        return polishWords;
+    }
 }
